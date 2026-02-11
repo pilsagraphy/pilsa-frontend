bc36a0c28214a0e1fcc54bc244ff48886c9cdc08
chae <nicky081719@gmail.com>
2026-02-11 19:02:52 +0900

    [FE] 회원가입 전 선택지 페이지 마크업 #19

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 // app/page.js (컴포넌트 테스트용)
-import StudentStatusSelector from '@/components/service/StudentStatusSelector';
+import PreSignup from '@/components/service/PreSignup';
 
 export default function MainPage() {
   return (
     <section className="mx-auto flex w-full max-w-md flex-col gap-6">
       <div className="space-y-2">
-        <StudentStatusSelector />
+        <PreSignup />
       </div>
     </section>
   );
