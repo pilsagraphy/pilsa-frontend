c3974ebfd8787f185610dccfeb4956bf53bc33a5
chae <nicky081719@gmail.com>
2026-02-11 19:32:09 +0900

    [FE] 회원가입 전 선택지 페이지 마크업 #19

--- app/page.js
+++ app/page.js
@@ -1,12 +1,8 @@
 // app/page.js (컴포넌트 테스트용)
-import PreSignup from '@/components/service/PreSignup';
-
 export default function MainPage() {
   return (
-    <section className="mx-auto flex w-full max-w-md flex-col gap-6">
-      <div className="space-y-2">
-        <PreSignup />
-      </div>
+    <section className="mx-auto flex w-full flex-col gap-6 ">
+      <div className="space-y-2">{/* Test 코드입력!*/}</div>
     </section>
   );
 }
