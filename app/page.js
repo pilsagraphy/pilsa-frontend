70aa5427d3413f353a24c2280f7bc56779fff6b6
chae <nicky081719@gmail.com>
2026-02-17 03:20:27 +0900

    🎨 [FE] 회원가입 페이지 마크업

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
+import Signup from '@/components/service/Signup';
 // app/page.js (컴포넌트 테스트용)
 export default function MainPage() {
   return (
     <section className="mx-auto flex w-full flex-col gap-6 ">
-      <div className="space-y-2">{/* Test 코드입력!*/}</div>
+      <div className="space-y-2">
+        <Signup />
+      </div>
     </section>
   );
 }
