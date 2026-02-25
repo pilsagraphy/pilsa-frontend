24a8a90be974dd27cd88906b23b6f23681ff5d84
yeabhin37 <dpqls7702@khu.ac.kr>
2026-02-25 14:15:57 +0900

    코멘트 반영

--- app/(public)/about/intro/page.js
+++ app/(public)/about/intro/page.js
@@ -1,10 +1,4 @@
 import Intro from "@/components/service/intro/Intro";
 export default function IntroPage() {
-  return (
-    <section className="mx-auto flex w-full flex-col gap-8">
-      <div className="space-y-2">
-        <Intro />
-      </div>
-    </section>
-  );
+  return <Intro />;
 }
