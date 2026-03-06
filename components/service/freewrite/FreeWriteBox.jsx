c8a27a5e230a82c900e372f076ad3ecbb822ba34
juhwan12345 <jjhwan0319@gmail.com>
2026-03-06 17:46:06 +0900

    코멘트 반영

--- components/service/freewrite/FreeWriteBox.jsx
+++ components/service/freewrite/FreeWriteBox.jsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import { useFreeWriteStore } from '../../../stores/freewrite.store';
+import { useFreeWriteStore } from '@/stores/freewrite.store';
 
 export default function FreeWriteBox({
   label,
