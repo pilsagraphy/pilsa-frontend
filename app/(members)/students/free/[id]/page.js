42c7cb316cab334ddd6502a4b45eaeb2e6a29d41
psns <psns0122@naver.com>
2026-03-18 02:59:00 +0900

    🚑 [FE] 로그인 유지 반영

--- app/(members)/students/free/[id]/page.js
+++ app/(members)/students/free/[id]/page.js
@@ -1,8 +1,9 @@
 import FreeDetailView from '@/components/service/freeDetail/FreeDetailView';
 import AuthGuard from '@/components/common/AuthGuard';
 
-export default function FreeDetailPage({ params }) {
-  const { id } = params;
+export default async function FreeDetailPage({ params }) {
+  const { id } = await params;
+
   return (
     <AuthGuard>
       <FreeDetailView postId={id} />
