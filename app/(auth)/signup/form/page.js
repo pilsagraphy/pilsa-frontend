a3c6960749d9c5fdc79b6063e5b45f437fcc31fb
psns <psns0122@naver.com>
2026-03-18 00:07:21 +0900

    🚑 [FE] 대소문자 핫픽스

--- app/(auth)/signup/form/page.js
+++ app/(auth)/signup/form/page.js
@@ -1,5 +1,5 @@
 // app/(auth)/signup/form/page.js
-import SignupForm from '@/components/service/signup/SignupForm';
+import SignupForm from '@/components/service/Signup/SignupForm';
 import { Suspense } from 'react';
 
 export default function SignupFormPage() {
--- app/(members)/students/notices/write/page.js
+++ app/(members)/students/notices/write/page.js
@@ -1,4 +1,4 @@
-import NoticeWrite from '@/components/service/noticeWrite/NoticeWrite';
+import NoticeWrite from '@/components/service/noticewrite/NoticeWrite';
 import AuthGuard from '@/components/common/AuthGuard';
 export default function NoticeWritePage() {
   return (
